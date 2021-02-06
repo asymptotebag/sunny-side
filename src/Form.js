6e30fc7dfd60c841c0c80ab5ae089623589805c5
kellyq17 <55288693+kellyq17@users.noreply.github.com>
2021-02-06 09:33:51 -0500

    Updatable Table Test

---
+++ src/Form.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div id="Form">
+        <h3>Add a new item to the table:</h3>  
+        <form onSubmit={this.props.handleFormSubmit}>
+          <label htmlFor="eventName">
+          Event:
+          <input id="eventName" value={this.props.newEvent} 
+            type="text" name="eventName"
+            onChange={this.props.handleInputChange} />
+          </label>
+
+          <label for="time">
+          Time:
+          <input id="time" value={this.props.newTime} 
+            type="time" name="time"
+            onChange={this.props.handleInputChange} />
+          </label>
+
+          <label for="date">
+          Date:
+          <input id="date" value={this.props.newDate} 
+            type="date" name="date"
+            onChange={this.props.handleInputChange} />
+          </label>
+          <button type="submit" value="Submit">Add Event</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
---
+++ src/Table.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Table extends React.Component {
+  render() {
+    const items = this.props.items;
+    return (
+      <div id="Table">
+        <table>
+          <tbody>
+            <tr>
+              <th>Event</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+            {items.map(item => {
+              return (
+                <tr>
+                  <td>{item.eventName}</td>
+                  <td>{item.date}</td>
+                  <td>{item.time}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Table;
